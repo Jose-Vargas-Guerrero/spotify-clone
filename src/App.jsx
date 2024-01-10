081ad40411f33d60ad7db483e85e8b3192f0402f
Jose-Vargas-Guerrero <81527579+Jose-Vargas-Guerrero@users.noreply.github.com>
2024-01-09 21:29:55 -0600

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Aside from './components/Aside'
 import PlayBar from './components/PlayBar'
-import Hero from './components/hero'
+import Hero from '/src/components/Hero'
 
 
 function App() {
